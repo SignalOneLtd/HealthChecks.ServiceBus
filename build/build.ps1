8ea525af281e3c78d656187f90a77d9ff4b970c0
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2018-10-18 19:47:08 +0100

    Tidy up

--- build/build.ps1
+++ build/build.ps1
@@ -4,11 +4,9 @@ function build($target, $version)
 	dotnet build $target -c Release /p:SourcesPackage=true /p:Version=$version-pre /p:PackageVersion=$version-pre
 }
 
-cls
-
 $version = "0.0.1"
 
-#build "..\src\SignalOne.HealthChecks.ServiceBus.Amazon" $version
+build "..\src\SignalOne.HealthChecks.ServiceBus.Amazon" $version
 build "..\src\SignalOne.HealthChecks.ServiceBus.Azure" $version
-#build "..\src\SignalOne.HealthChecks.ServiceBus.Kafka" $version
-#build "..\src\SignalOne.HealthChecks.ServiceBus.RabbitMQ" $version
+build "..\src\SignalOne.HealthChecks.ServiceBus.Kafka" $version
+build "..\src\SignalOne.HealthChecks.ServiceBus.RabbitMQ" $version
