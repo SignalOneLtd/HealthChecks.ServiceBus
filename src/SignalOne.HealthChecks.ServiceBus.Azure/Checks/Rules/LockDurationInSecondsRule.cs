c03c473f1562c1aecdc10efd1bda2bd1ad8a1ff4
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2018-10-23 18:15:52 +0100

    Added LockDurationInSecondsRule

---
+++ src/SignalOne.HealthChecks.ServiceBus.Azure/Checks/Rules/LockDurationInSecondsRule.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Management.ServiceBus.Fluent;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules
+{
+    internal sealed class LockDurationInSecondsRule : IQueueRule, ISubscriptionRule
+    {
+        public IEnumerable<(string Key, string Error)> ValidateResource(IQueue resource, QueueHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.LockDurationInSeconds, resource.LockDurationInSeconds);
+        }
+
+        public IEnumerable<(string Key, string Error)> ValidateResource(ISubscription resource, SubscriptionHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.LockDurationInSeconds, resource.LockDurationInSeconds);
+        }
+
+        private static IEnumerable<(string Key, string Error)> Check(long? expected, long actual)
+        {
+            if (!expected.HasValue)
+                yield break;
+
+            if (!expected.Value.Equals(actual))
+                yield return ("LockDurationInSeconds", $"Expected Value: '{expected}', Actual Value: '{actual}'");
+        }
+    }
+}
