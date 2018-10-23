9120fbcb3c15a44a79485e170643fe68f7b98d62
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2018-10-23 18:16:25 +0100

    Added StatusRule

---
+++ src/SignalOne.HealthChecks.ServiceBus.Azure/Checks/Rules/StatusRule.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.Management.ServiceBus.Fluent;
+using Microsoft.Azure.Management.ServiceBus.Fluent.Models;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules
+{
+    internal sealed class StatusRule : IQueueRule, ITopicRule, ISubscriptionRule
+    {
+        public IEnumerable<(string Key, string Error)> ValidateResource(IQueue resource, QueueHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.Status, resource.Status);
+        }
+
+        public IEnumerable<(string Key, string Error)> ValidateResource(ITopic resource, TopicHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.Status, resource.Status);
+        }
+
+        public IEnumerable<(string Key, string Error)> ValidateResource(ISubscription resource, SubscriptionHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.Status, resource.Status);
+        }
+
+        private static IEnumerable<(string Key, string Error)> Check(EntityStatus? expected, EntityStatus actual)
+        {
+            if (!expected.HasValue)
+                yield break;
+
+            if (!expected.Value.Equals(actual))
+                yield return ("MaxSizeInMB", $"Expected Value: '{expected}', Actual Value: '{actual}'");
+        }
+    }
+}
---
+++ tests/SignalOne.HealthChecks.ServiceBus.Azure.Tests/Checks/Rules/StatusRuleTests.cs
@@ -0,0 +1,185 @@
+﻿using FluentAssertions;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
+using Microsoft.Azure.Management.ServiceBus.Fluent.Models;
+using Moq;
+using SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using Xunit;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Tests.Checks.Rules
+{
+    public class StatusRuleTests
+    {
+        public class WhenQueue
+        {
+            [Fact]
+            public void WhenResourceIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(default, new QueueHealthCheckOptions());
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("resource");
+            }
+
+            [Fact]
+            public void WhenOptionsIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(new Mock<IQueue>().Object, default);
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("options");
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndNoValueSupplied_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var queue = new Mock<IQueue>();
+
+                target.ValidateResource(queue.Object, new QueueHealthCheckOptions()).Should().HaveCount(0);
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreEqual_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var queue = new Mock<IQueue>();
+                var state = EntityStatus.Active;
+                queue.Setup(x => x.Status).Returns(() => state).Verifiable();
+
+                target.ValidateResource(queue.Object, new QueueHealthCheckOptions { Status = state }).Should().HaveCount(0);
+
+                queue.Verify();
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreNotEqual_SingleErrorReturned()
+            {
+                var target = new StatusRule();
+                var queue = new Mock<IQueue>();
+                queue.Setup(x => x.Status).Returns(() => EntityStatus.Active).Verifiable();
+
+                target.ValidateResource(queue.Object, new QueueHealthCheckOptions { Status = EntityStatus.Disabled }).Should().HaveCount(1);
+
+                queue.Verify();
+            }
+        }
+
+        public class WhenTopic
+        {
+            [Fact]
+            public void WhenResourceIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(default, new TopicHealthCheckOptions());
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("resource");
+            }
+
+            [Fact]
+            public void WhenOptionsIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(new Mock<ITopic>().Object, default);
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("options");
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndNoValueSupplied_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var topic = new Mock<ITopic>();
+
+                target.ValidateResource(topic.Object, new TopicHealthCheckOptions()).Should().HaveCount(0);
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreEqual_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var topic = new Mock<ITopic>();
+                var state = EntityStatus.Active;
+                topic.Setup(x => x.Status).Returns(() => state).Verifiable();
+
+                target.ValidateResource(topic.Object, new TopicHealthCheckOptions { Status = state }).Should().HaveCount(0);
+
+                topic.Verify();
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreNotEqual_SingleErrorReturned()
+            {
+                var target = new StatusRule();
+                var topic = new Mock<ITopic>();
+                topic.Setup(x => x.Status).Returns(() => EntityStatus.Active).Verifiable();
+
+                target.ValidateResource(topic.Object, new TopicHealthCheckOptions { Status = EntityStatus.Disabled }).Should().HaveCount(1);
+
+                topic.Verify();
+            }
+        }
+
+        public class WhenSubscription
+        {
+            [Fact]
+            public void WhenResourceIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(default, new SubscriptionHealthCheckOptions());
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("resource");
+            }
+
+            [Fact]
+            public void WhenOptionsIsNull_ThrowsArgumentNullException()
+            {
+                var target = new StatusRule();
+
+                Action act = () => target.ValidateResource(new Mock<ISubscription>().Object, default);
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("options");
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndNoValueSupplied_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var subscription = new Mock<ISubscription>();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions()).Should().HaveCount(0);
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreEqual_NoErrorsAreReturned()
+            {
+                var target = new StatusRule();
+                var subscription = new Mock<ISubscription>();
+                var state = EntityStatus.Active;
+                subscription.Setup(x => x.Status).Returns(() => state).Verifiable();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions { Status = state }).Should().HaveCount(0);
+
+                subscription.Verify();
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreNotEqual_SingleErrorReturned()
+            {
+                var target = new StatusRule();
+                var subscription = new Mock<ISubscription>();
+                subscription.Setup(x => x.Status).Returns(() => EntityStatus.Active).Verifiable();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions { Status = EntityStatus.Disabled }).Should().HaveCount(1);
+
+                subscription.Verify();
+            }
+        }
+    }
+}
