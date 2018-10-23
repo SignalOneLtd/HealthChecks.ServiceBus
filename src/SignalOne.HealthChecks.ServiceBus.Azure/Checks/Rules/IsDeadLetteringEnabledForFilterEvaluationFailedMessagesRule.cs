e25fcebe3a6118424de40e77bef6438f47f9de14
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2018-10-23 18:14:41 +0100

    Added IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule

---
+++ src/SignalOne.HealthChecks.ServiceBus.Azure/Checks/Rules/IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Management.ServiceBus.Fluent;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules
+{
+    internal sealed class IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule : ISubscriptionRule
+    {
+        public IEnumerable<(string Key, string Error)> ValidateResource(ISubscription resource, SubscriptionHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            if (!options.IsDeadLetteringEnabledForFilterEvaluationFailedMessages.HasValue)
+                yield break;
+
+            if (resource.IsDeadLetteringEnabledForFilterEvaluationFailedMessages != options.IsDeadLetteringEnabledForFilterEvaluationFailedMessages.Value)
+                yield return (nameof(options.IsDeadLetteringEnabledForFilterEvaluationFailedMessages), $"Expected Value: '{options.IsDeadLetteringEnabledForFilterEvaluationFailedMessages}', Actual Value: '{resource.IsDeadLetteringEnabledForFilterEvaluationFailedMessages}'");
+        }
+    }
+}
---
+++ tests/SignalOne.HealthChecks.ServiceBus.Azure.Tests/Checks/Rules/IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRuleTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
+using Moq;
+using SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Tests.Checks.Rules
+{
+    public class IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRuleTests
+    {
+        public class WhenSubscription
+        {
+            [Fact]
+            public void WhenResourceIsNull_ThrowsArgumentNullException()
+            {
+                var target = new IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule();
+
+                Action act = () => target.ValidateResource(default, new SubscriptionHealthCheckOptions()).ToList();
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("resource");
+            }
+
+            [Fact]
+            public void WhenOptionsIsNull_ThrowsArgumentNullException()
+            {
+                var target = new IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule();
+
+                Action act = () => target.ValidateResource(new Mock<ISubscription>().Object, default).ToList();
+
+                act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("options");
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndNoValueSupplied_NoErrorsAreReturned()
+            {
+                var target = new IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule();
+                var subscription = new Mock<ISubscription>();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions()).Should().HaveCount(0);
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreEqual_NoErrorsAreReturned()
+            {
+                var target = new IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule();
+                var subscription = new Mock<ISubscription>();
+                var enabled = true;
+                subscription.Setup(x => x.IsDeadLetteringEnabledForFilterEvaluationFailedMessages).Returns(() => enabled).Verifiable();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions { IsDeadLetteringEnabledForFilterEvaluationFailedMessages = enabled }).Should().HaveCount(0);
+
+                subscription.Verify();
+            }
+
+            [Fact]
+            public void WhenRuleSupplied_AndValuesAreNotEqual_SingleErrorReturned()
+            {
+                var target = new IsDeadLetteringEnabledForFilterEvaluationFailedMessagesRule();
+                var subscription = new Mock<ISubscription>();
+                subscription.Setup(x => x.IsDeadLetteringEnabledForFilterEvaluationFailedMessages).Returns(() => true).Verifiable();
+
+                target.ValidateResource(subscription.Object, new SubscriptionHealthCheckOptions { IsDeadLetteringEnabledForFilterEvaluationFailedMessages = false }).Should().HaveCount(1);
+
+                subscription.Verify();
+            }
+        }
+    }
+}
