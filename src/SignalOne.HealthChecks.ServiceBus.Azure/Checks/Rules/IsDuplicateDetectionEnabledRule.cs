c8f2aaaff32ec43533e96ce1c4329010bbe8a62b
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2018-10-23 18:14:56 +0100

    Added IsDuplicateDetectionEnabledRule

---
+++ src/SignalOne.HealthChecks.ServiceBus.Azure/Checks/Rules/IsDuplicateDetectionEnabledRule.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Management.ServiceBus.Fluent;
+using SignalOne.HealthChecks.ServiceBus.Azure.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace SignalOne.HealthChecks.ServiceBus.Azure.Checks.Rules
+{
+    internal sealed class IsDuplicateDetectionEnabledRule : IQueueRule, ITopicRule
+    {
+        public IEnumerable<(string Key, string Error)> ValidateResource(IQueue resource, QueueHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.IsDuplicateDetectionEnabled, resource.IsDuplicateDetectionEnabled);
+        }
+
+        public IEnumerable<(string Key, string Error)> ValidateResource(ITopic resource, TopicHealthCheckOptions options)
+        {
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return Check(options.IsDuplicateDetectionEnabled, resource.IsDuplicateDetectionEnabled);
+        }
+
+        private static IEnumerable<(string Key, string Error)> Check(bool? expected, bool actual)
+        {
+            if (!expected.HasValue)
+                yield break;
+
+            if (!expected.Value.Equals(actual))
+                yield return ("IsDuplicateDetectionEnabled", $"Expected Value: '{expected}', Actual Value: '{actual}'");
+        }
+    }
+}
